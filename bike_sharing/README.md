601ea73ded5612dea283b49b1253ffa789625f6e
Ben Johnson <benjohnson@Bens-MacBook-Pro.local>
2021-11-09 19:29:33 -0800

    clean: remove report

---
+++ bike_sharing/README.md
@@ -0,0 +1,4 @@
+# Bike Sharing Analysis
+
+## Scripts
+Note all scripts are intended to be executed at the root of the project, in `bike_sharing`.
--- bike_sharing/scripts/prior-prediction.R
+++ bike_sharing/scripts/prior-prediction.R
@@ -1,19 +1,32 @@
 library(rethinking)
 
 # set destination report file
-pdf(file = '../reports/prior_prediction.pdf')
+
+#png(file = 'reports/sample_x.png',
+#    width     = 3.25,
+#    height    = 3.25,
+#    units     = "in",
+#    res       = 1200,
+#    pointsize = 4
+#    )
+
+
+pdf(file = 'reports/prior_prediction.pdf')
+
 
 # construct priors
-sample_a <- rlnorm(1e4, 0, 100)
-sample_b <- rlnorm(1e4, 500, 200)
+sample_a <- rlnorm(1e4, 0, 1)
+sample_b <- rnorm(1e4, 500, 200)
 sample_s <- runif(1e4, 0, 200)
 
-# joint prior
-prior_h <- rnorm( 1e4 , sample_a + sample_b * 0.5 , sample_s )
 
-plot(sample_a)
-plot(sample_b)
-plot(sample_s)
+# joint prior
+prior_x <- rnorm( 1e4 , sample_a + sample_b * 0.8 , sample_s )
+
+plot(dens(sample_a))
+#plot(sample_b)
+#plot(dens(sample_s))
+#plot(dens(prior_x))
 
 # turn off plotting
 dev.off()
